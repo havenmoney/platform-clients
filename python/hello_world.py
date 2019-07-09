56e6875a027b1ac809e5d58b19fa2e70f00b42ca
Sloane Sturzenegger <sloane@havenmoney.com>
2019-07-08 17:44:16 -0700

    Haven-specific python client and hello-world

---
+++ python/hello_world.py
@@ -0,0 +1,36 @@
+import haven
+from haven.authed_api_client import AuthedApiClient
+
+from datetime import datetime
+from dateutil.tz import tzutc
+
+
+def main():
+    config = haven.Configuration(host="https://haven.dev/api")
+    api = haven.DefaultApi(AuthedApiClient(
+        id="YOUR_ID_HERE",
+        secret="YOUR_SECRET_HERE",
+        configuration=config))
+
+    # upload a transaction
+    api.upsert_one_transaction(haven.CleanSingleTransactionRequest(
+        transaction=haven.RawTransaction(
+            id="txn-1",
+            account="account-a",
+            user="user-0",
+            raw_memo="Uber ***232**123",
+            merchant=None,
+            store=None,
+            network="VISA",
+            user_transacted_at=datetime.now().replace(tzinfo=tzutc()),
+            amount="-12.34",
+            categories=[],
+            kind=haven.TransactionKind.CREDIT_CARD,
+            state=haven.TransactionState.COMPLETED)))
+
+    transactions = api.get_transactions("user-0")
+    print(transactions)
+
+
+if __name__ == "__main__":
+    main()
